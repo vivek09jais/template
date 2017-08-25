fdb7d297758fd51210506bad77c9fa1e11d358d0
vivek kumar <vivekjaiswal1111@redifmail.com>
2017-08-25 09:59:02 +0530

    Initial Template Commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.zoom').hover(function() {
+        $(this).addClass('transition');
+    }, function() {
+        $(this).removeClass('transition');
+    });
+});
+
+
+
+
